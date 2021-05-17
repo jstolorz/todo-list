a8bdebc28490037ac4eef03b0f52cfed74cd0adf
janusz <jstolorz@gmail.com>
2021-05-17 17:48:00 +0200

    Adding Request parameter and use it.

--- src/main/java/com/bluesoft/controller/DemoController.java
+++ src/main/java/com/bluesoft/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -27,8 +28,12 @@ public class DemoController {
     }
 
     @GetMapping("welcome")
-    public String welcome(Model model){
-        model.addAttribute("helloMessage",service.getHelloMessage("janusz"));
+    public String welcome(
+            @RequestParam String user,
+            @RequestParam int age,
+            Model model){
+        model.addAttribute("helloMessage",service.getHelloMessage(user));
+        model.addAttribute("age",age);
         return "welcome";
     }
 
--- src/main/webapp/WEB-INF/view/welcome.jsp
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -5,7 +5,8 @@
     <title>Welcome</title>
 </head>
 <body>
-  From welcome.jsp -- ${helloMessage}
+  <p>From welcome.jsp -- ${helloMessage}</p>
+  <p>Age parameter is ${age}</p>
   <h2>${welcomeMessage}</h2>
 </body>
 </html>
